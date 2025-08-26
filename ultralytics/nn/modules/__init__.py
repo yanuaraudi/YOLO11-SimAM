3eeb124fc1924996923ace08176d865a8a485edf
Yanuar Audi Nugraha <Yanuar Audi Nugraha>
2025-08-26 07:20:47 +0700

    fix: the naming package to also include directory details as well

--- ultralytics/nn/modules/__init__.py
+++ ultralytics/nn/modules/__init__.py
@@ -16,7 +16,7 @@ Examples:
     >>> torch.onnx.export(m, x, f)
     >>> os.system(f"onnxslim {f} {f} && open {f}")  # pip install onnxslim
 """
-from custom_modules import SimAM
+from .custom_modules import SimAM
 from .block import (
     C1,
     C2,
